9ee1e02fec4bd9b5dd50dd8cd72a26229bd36846
LuckyAnggara <anggara.lucky1992@gmail.com>
2022-12-14 15:44:58 +0700

    seep

--- app.js
+++ app.js
@@ -3,56 +3,105 @@ const cheerio = require('cheerio')
 const express = require('express')
 const puppeteer = require('puppeteer')
 
-const port = process.env.port || 3000
+const port = process.env.port || 3001
 
 const app = express()
 
 async function getLastUpdate() {
+  // we can block by resrouce type like fonts, images etc.
+  const blockResourceType = ['beacon', 'csp_report', 'font', 'image', 'imageset', 'media', 'object', 'texttrack', 'stylesheet']
+  // we can also block by domains, like google-analytics etc.
+
   const data = []
   const no = 1
   const url = 'https://mangakyo.id/'
 
-  try {
-    await axios
-      .get(url, {
-        headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
-      })
-      .then((response) => {
-        const html_data = response.data
-        const $ = cheerio.load(html_data)
-        const selectedElement = 'div.styletere'
-        $(selectedElement).each((index, el) => {
-          const lastUpdate = []
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.setRequestInterception(true)
 
-          const body = $(el).find('.tt').find('a')
-          const img = $(el)
-            .find('img.ts-post-image')
-            .attr('data-src')
-            .replace('?resize=165,225', '')
+  // then we can add a call back which inspects every
+  // outgoing request browser makes and decides whether to allow it
+  page.on('request', (req) => {
+    if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+      req.abort()
+    } else {
+      req.continue()
+    }
+  })
 
-          $(el)
-            .find('.chfiv')
-            .find('a')
-            .each((a, b) => {
-              lastUpdate.push({
-                chapter: $(b).text(),
-                href: $(b).attr('href').replace('https://mangakyo.id', ''),
-              })
-            })
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
+  await page.goto(url, { waitUntil: 'domcontentloaded' })
+  await page.screenshot({ path: 'screenshot.png' })
+  const mangas = await page.$$('.bs.styletere.stylefiv div.bsx')
+  for (const manga of mangas) {
+    data.push(
+      await manga.evaluate((node) => {
+        const lastUpdate = []
+        const updates = node.querySelector('.bigor .chfiv').querySelectorAll('li')
 
-          data.push({
-            no: index + no,
-            title: body.attr('title'),
-            href: body.attr('href').replace('https://mangakyo.id/komik', ''),
-            img: img,
-            lastUpdate: lastUpdate,
+        for (const update of updates) {
+          lastUpdate.push({
+            chapter: update.querySelector('a').innerText,
+            time: update.querySelector('span').innerText,
+            href: update.querySelector('a').getAttribute('href'),
           })
-        })
+        }
+
+        return {
+          title: node.querySelector('a').getAttribute('title'),
+          href: node.querySelector('a').getAttribute('href').replace('https://mangakyo.id/komik', ''),
+          image: node.querySelector('a img').getAttribute('src'),
+          lastUpdate: lastUpdate,
+        }
       })
-    return data
-  } catch (error) {
-    return error
+    )
   }
+
+  browser.close()
+  // try {
+  //   await axios
+  //     .get(url, {
+  //       headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+  //     })
+  //     .then((response) => {
+  //       const html_data = response.data
+  //       data.push({
+  //         a: response.data,
+  //       })
+  //       const $ = cheerio.load(html_data)
+  //       const selectedElement = '.styletere'
+  //       $(selectedElement).each((index, el) => {
+  //         const lastUpdate = []
+
+  //         const body = $(el).find('.tt').find('a')
+  //         const img = $(el).find('img.ts-post-image').attr('data-src').replace('?resize=165,225', '')
+
+  //         // $(el)
+  //         //   .find('.chfiv')
+  //         //   .find('a')
+  //         //   .each((a, b) => {
+  //         //     lastUpdate.push({
+  //         //       chapter: $(b).text(),
+  //         //       href: $(b).attr('href').replace('https://mangakyo.id', ''),
+  //         //     })
+  //         //   })
+
+  //         data.push({
+  //           no: 'aa',
+  //           // title: body.attr('title'),
+  //           // href: body.attr('href').replace('https://mangakyo.id/komik', ''),
+  //           // img: img,
+  //           // lastUpdate: lastUpdate,
+  //         })
+  //       })
+  //     })
+  //   return data
+  // } catch (error) {
+  //   return error
+  // }
+
+  return data
 }
 
 async function search(name) {
@@ -72,10 +121,7 @@ async function search(name) {
         $(selectedElement).each((index, el) => {
           const body = $(el).find('a')
           const lastChapter = $(el).find('.epxs').text()
-          const img = $(el)
-            .find('img')
-            .attr('src')
-            .replace('?resize=165,225', '')
+          const img = $(el).find('img').attr('src').replace('?resize=165,225', '')
 
           data.push({
             no: index + no,
@@ -132,10 +178,7 @@ async function detail(link) {
         $('#chapterlist li').each((a, b) => {
           chapter.push({
             name: $(b).attr('data-num'),
-            href: $(b)
-              .find('a')
-              .attr('href')
-              .replace('https://mangakyo.id', ''),
+            href: $(b).find('a').attr('href').replace('https://mangakyo.id', ''),
           })
         })
         data.detail = {
@@ -163,9 +206,17 @@ async function read(link) {
 
   const browser = await puppeteer.launch({ headless: true })
   let page = await browser.newPage()
-  await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-  )
+  await page.setRequestInterception(true)
+
+  page.on('request', (req) => {
+    if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+      req.abort()
+    } else {
+      req.continue()
+    }
+  })
+
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
   await page.goto(url, { waitUntil: 'domcontentloaded' })
   await page.waitForSelector('.ts-main-image').then(() => console.log('loaded'))
   await page.screenshot({ path: 'screenshot.jpg' })
